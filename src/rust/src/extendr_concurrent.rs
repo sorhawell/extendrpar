92e0403790497e15ff6a004746c52d5737172184
sorhawell <sorhawell@gmail.com>
2022-09-23 10:42:29 +0200

    deprecate predictive sleep pattern

--- src/rust/src/extendr_concurrent.rs
+++ src/rust/src/extendr_concurrent.rs
@@ -6,7 +6,6 @@ use std::thread;
 
 use flume;
 use flume::{Receiver, Sender};
-
 use state::Storage;
 
 //shamelessly make Robj send + sync
@@ -37,7 +36,6 @@ impl<S, R> ThreadCom<S, R> {
     pub fn create() -> (Self, Receiver<(S, Sender<R>)>) {
         let (m_tx, m_rx) = flume::unbounded::<(S, Sender<R>)>();
         let (c_tx, c_rx) = flume::unbounded::<R>();
-
         (
             ThreadCom {
                 mains_tx: m_tx,
@@ -47,22 +45,6 @@ impl<S, R> ThreadCom<S, R> {
             m_rx,
         )
     }
-
-    // pub fn from_main_sender(
-    //     mc: (Sender<(S, Sender<R>)>, Receiver<(S, Sender<R>)>),
-    // ) -> (Self, flume::Receiver<(S, flume::Sender<R>)>) {
-    //     let (m_tx, m_rx) = (mc.0, mc.1);
-    //     let (c_tx, c_rx) = flume::unbounded::<R>();
-
-    //     (
-    //         ThreadCom {
-    //             mains_tx: m_tx,
-    //             child_tx: c_tx,
-    //             child_rx: c_rx,
-    //         },
-    //         m_rx,
-    //     )
-    // }
 
     //send request to main thread
     pub fn send(&self, s: S) {
@@ -77,6 +59,7 @@ impl<S, R> ThreadCom<S, R> {
     //clone only main unbounded, create new unique child unbounded (such that each thread has unique comminucation when main)
     pub fn clone(&self) -> Self {
         let (tx, rx) = flume::unbounded::<R>();
+
         ThreadCom {
             mains_tx: self.mains_tx.clone(),
             child_rx: rx,
@@ -85,7 +68,17 @@ impl<S, R> ThreadCom<S, R> {
     }
 }
 
-pub fn concurrent_handler<F, I, R, S, T>(f: F, i: I, conf: &Storage<RwLock<ThreadCom<S, R>>>) -> T
+impl<S, R> Drop for ThreadCom<S, R> {
+    fn drop(&mut self) {
+        println!("Dropping a threadcom somewhere");
+    }
+}
+
+pub fn concurrent_handler<F, I, R, S, T>(
+    f: F,
+    i: I,
+    conf: &Storage<RwLock<Option<ThreadCom<S, R>>>>,
+) -> T
 where
     F: FnOnce(ThreadCom<S, R>) -> T + Send + 'static,
     I: Fn(S) -> R + Send + 'static,
@@ -93,19 +86,19 @@ where
     S: Send + 'static + std::fmt::Display,
     T: Send + 'static,
 {
-    //start cßom unboundeds
+    //start com unboundeds
 
     let (thread_com, main_rx) = ThreadCom::create();
 
     //set or update global thread_com
-    let conf_status = conf.set(RwLock::new(thread_com.clone()));
+    let conf_status = conf.set(RwLock::new(Some(thread_com.clone())));
     dbg!(conf_status);
     if !conf_status {
         let mut gtc = conf
             .get()
             .write()
             .expect("failed to modify GLOBAL therad_com");
-        *gtc = thread_com.clone();
+        *gtc = Some(thread_com.clone());
     }
 
     //start child thread(s)
@@ -113,7 +106,7 @@ where
 
     //serve any request from child threads until all child_phones are dropped or R interrupt
     let mut before = std::time::Instant::now();
-    let mut planned_sleep = std::time::Duration::from_micros(2);
+    let start_time = std::time::Instant::now();
     let mut loop_counter = 0u64;
     loop {
         loop_counter += 1;
@@ -121,59 +114,41 @@ where
         let duration = now - before;
         before = std::time::Instant::now();
         dbg!(duration, loop_counter);
-        //look for
-        let any_new_msg = main_rx.try_recv();
+
+        let any_new_msg = main_rx.recv_timeout(std::time::Duration::from_millis(100));
 
         //avoid using unwrap/unwrap_err if msg is Debug
         if let Ok(packet) = any_new_msg {
             let (s, c_tx) = packet;
-            let answer = i(s); //handle requst with g closure
+            let answer = i(s); //handle requst with i closure
             let _send_result = c_tx.send(answer).unwrap();
-
-            //stuff to do!! sleep less if ever idle
-            planned_sleep =
-                std::time::Duration::max(planned_sleep / 4, std::time::Duration::from_nanos(100));
         } else {
             if let Err(recv_err) = any_new_msg {
                 dbg!(recv_err);
                 match recv_err {
                     //no connections left, shut down loop, does not happen after one global tx always exists
                     //in theory a thread or main could destroy global thread_com to terminate this way
-                    flume::TryRecvError::Disconnected => {
+                    flume::RecvTimeoutError::Disconnected => {
                         dbg!(&recv_err);
                         break;
                     }
                     //idling, sleep double as long as last time
-                    flume::TryRecvError::Empty => {
+                    flume::RecvTimeoutError::Timeout => {
                         //check for R user interrupt with Sys.sleep(0) every 1 second if not serving a thread.
 
-                        if duration >= std::time::Duration::from_secs(1) {
-                            before = now;
-                            let res_res =
-                                extendr_api::eval_string(&"print('check user');Sys.sleep(0)");
-                            if res_res.is_err() {
-                                rprintln!("user interrupt");
-                                break;
-                            }
+                        let res_res = extendr_api::eval_string(&"print('check user');Sys.sleep(0)");
+                        if res_res.is_err() {
+                            rprintln!("R user signalled interrupt");
+                            break;
                         }
 
-                        //check if spawned thread has ended, then stop. (most normal end)
+                        //TODO sub-optimal way to finish, better if secondlast threadcom destroys the global threadcom
+                        //thereby notifying to main thread time to disconnect, could save 50-100ms timeout wait
                         if handle.is_finished() {
                             dbg!(&handle);
                             break;
                         }
-
-                        //sleep thead takes 50-100 micros also
-                        if planned_sleep > std::time::Duration::from_micros(60) {
-                            dbg!(planned_sleep);
-                            thread::sleep(planned_sleep);
-                        }
-
-                        planned_sleep = std::time::Duration::min(
-                            planned_sleep * 4 / 3,
-                            std::time::Duration::from_millis(5),
-                        );
-                    }
+                    } //check if spawned thread has ended, then stop. (most normal end)
                 }
             } else {
                 unreachable!("a result was neither error or ok");
@@ -182,6 +157,9 @@ where
     }
 
     let thread_return_value = handle.join().unwrap();
+
+    let run_time = std::time::Instant::now() - start_time;
+    dbg!(run_time);
 
     thread_return_value
 }
--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -8,7 +8,7 @@ use std::sync::RwLock;
 use std::thread;
 
 use state::Storage;
-static CONFIG: Storage<RwLock<ThreadCom<String, ParRObj>>> = Storage::new();
+static CONFIG: Storage<RwLock<Option<ThreadCom<String, ParRObj>>>> = Storage::new();
 
 #[extendr]
 fn par_con_handler() -> i32 {
@@ -20,13 +20,15 @@ fn par_con_handler() -> i32 {
             let _answer: ParRObj = thread_com.recv();
 
             let mut v = Vec::new();
-            for i in 0..1 {
+            for _ in 0..3 {
+                thread::sleep(std::time::Duration::from_millis(3));
                 //let tc_clone = thread_com.clone();
 
                 let h = thread::spawn(move || {
                     let mut vv = Vec::new();
                     //let thread_com = tc_clone;
-                    for j in 0..1 {
+                    for j in 0..3 {
+                        thread::sleep(std::time::Duration::from_millis(3));
                         //oups forgot to bring thread_com
                         //let tc_clone = thread_com.clone();
                         let hh = thread::spawn(move || {
@@ -34,15 +36,14 @@ fn par_con_handler() -> i32 {
                             let thread_com = CONFIG
                                 .get()
                                 .read()
-                                .expect("failded to restore thread_com")
+                                .expect("failded to acquire thread_com")
+                                .as_ref()
+                                .unwrap()
                                 .clone();
 
-                            for k in 0..50 {
-                                //thread::sleep(std::time::Duration::from_millis(1));
-                                thread_com.send(format!(
-                                    "print('hej from thread_{}');1:4",
-                                    1 + k + j * 25 + i * 2
-                                ));
+                            for _ in 0..3 {
+                                thread::sleep(std::time::Duration::from_millis(15));
+                                thread_com.send(format!("print('hej from thread_{}');1:4", j));
                                 let _answer: ParRObj = thread_com.recv();
                             }
                             // {
@@ -65,6 +66,18 @@ fn par_con_handler() -> i32 {
                     .into_iter()
                     .for_each(|hh| hh.join().unwrap())
             });
+
+            //this is the last thread returning, all child thread can be assumed to have finnished
+            //kill last threadcom to notify mainthread
+            let mut val = CONFIG
+                .get()
+                .write()
+                .expect("another thread crashed while touching CONFIG");
+            dbg!("killing CONFIG threadcom");
+            *val = None;
+
+            dbg!("killing very last threadcom");
+            drop(thread_com);
 
             //thread return is T
             42i32
